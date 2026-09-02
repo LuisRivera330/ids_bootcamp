05737b2d41238ca55d69ff09041fe56595d57500
Luis Johan Rivera Gonzales <coartac.luis.rivera@gmail.com>
2026-09-02 17:44:21 -0500

    Día 12 Java

--- java/chapter_05_class_design/d11_creating_abstract_classes/creating_a_concrete_class/Animal.java
+++
@@ -1,5 +0,0 @@
-package ids_bootcamp.java.chapter_05_class_design.d11_creating_abstract_classes.creating_a_concrete_class;
-
-public abstract class Animal {
-    public abstract String getName();
-}
--- java/chapter_05_class_design/d11_creating_abstract_classes/extending_an_abstract_class/Walrus.java
+++
@@ -1,4 +0,0 @@
-package ids_bootcamp.java.chapter_05_class_design.d11_creating_abstract_classes.extending_an_abstract_class;
-
-public class Walrus extends Animal { // No compila porque, no se implementan todos los métodos abtractos de Animal
-}
